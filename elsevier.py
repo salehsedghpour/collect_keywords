d39a7307b2d4925be4121621f643abb8570e150e
mr.salehsedghpour@yahoo.com <mr.salehsedghpour@yahoo.com>
2019-03-31 16:44:36 +0430

    Final Solution

--- elsevier.py
+++ elsevier.py
@@ -18,7 +18,12 @@ for word in words:
         temp = temp +'%20'+ item_temp
     r = session.get("https://www.sciencedirect.com/search?qs=%27"+temp+"%27&show=25&sortBy=relevance&years=2019%2C2018%2C2020&lastSelectedFacet=articleTypes&articleTypes=FLA%2CREV")
     #r.html.render(timeout=30)
+    #print("https://www.sciencedirect.com/search?qs=%27"+temp+"%27&show=25&sortBy=relevance&years=2019%2C2018%2C2020&lastSelectedFacet=articleTypes&articleTypes=FLA%2CREV")
     #r.html.find('span.search-body-results-text').text
-    print(r.html.find('span.search-body-results-text')[0].text)
+
+    if r.html.find('span.search-body-results-text') != []:
+        print(r.html.find('span.search-body-results-text')[0].text.split()[0].replace(',',''))
+    else:
+        print(0)
 
     #print(r.html.find('span'))
